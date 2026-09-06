2cb0038be9ed850956b5a12e7bcea4e42937bcff
hutong236 <4711428+hutong236@users.noreply.github.com>
2026-09-06 13:52:17 +0800

    test: cover non-conflicting desktop commands

--- Tests/OpsNotchCoreTests/DesktopCommandParserTests.swift
+++ Tests/OpsNotchCoreTests/DesktopCommandParserTests.swift
@@ -2,26 +2,30 @@ import XCTest
 @testable import OpsNotchCore
 
 final class DesktopCommandParserTests: XCTestCase {
-    func testParsesDesktopSelectionCommands() {
-        XCTAssertEqual(DesktopCommandParser.parse("d1"), .switchTo(index: 1))
-        XCTAssertEqual(DesktopCommandParser.parse("D3"), .switchTo(index: 3))
-        XCTAssertEqual(DesktopCommandParser.parse("d12"), .switchTo(index: 12))
+    func testParsesSpacedDesktopSelectionCommands() {
+        XCTAssertEqual(DesktopCommandParser.parse("d 1"), .switchTo(index: 1))
+        XCTAssertEqual(DesktopCommandParser.parse("D 3"), .switchTo(index: 3))
+        XCTAssertEqual(DesktopCommandParser.parse("d    12"), .switchTo(index: 12))
         XCTAssertEqual(DesktopCommandParser.parse("desktop 4"), .switchTo(index: 4))
         XCTAssertEqual(DesktopCommandParser.parse("桌面 5"), .switchTo(index: 5))
     }
 
-    func testParsesDesktopListCommands() {
+    func testParsesDesktopListSuggestions() {
         XCTAssertEqual(DesktopCommandParser.parse("d"), .list)
         XCTAssertEqual(DesktopCommandParser.parse(" desktop "), .list)
         XCTAssertEqual(DesktopCommandParser.parse("桌面"), .list)
     }
 
-    func testRejectsAmbiguousOrInvalidCommands() {
+    func testRejectsCompactOrAmbiguousSearchTerms() {
         XCTAssertNil(DesktopCommandParser.parse(""))
-        XCTAssertNil(DesktopCommandParser.parse("d0"))
-        XCTAssertNil(DesktopCommandParser.parse("d01"))
+        XCTAssertNil(DesktopCommandParser.parse("d1"))
+        XCTAssertNil(DesktopCommandParser.parse("d2"))
+        XCTAssertNil(DesktopCommandParser.parse("d 0"))
+        XCTAssertNil(DesktopCommandParser.parse("d 01"))
         XCTAssertNil(DesktopCommandParser.parse("dx"))
-        XCTAssertNil(DesktopCommandParser.parse("download"))
-        XCTAssertNil(DesktopCommandParser.parse("my d1"))
+        XCTAssertNil(DesktopCommandParser.parse("ddd"))
+        XCTAssertNil(DesktopCommandParser.parse("docker"))
+        XCTAssertNil(DesktopCommandParser.parse("desktop app"))
+        XCTAssertNil(DesktopCommandParser.parse("my d 1"))
     }
 }
